968e13899a5904c56bca646892d670a979d2a2f0
marceloroot <marcelo.lima.gomes.23@gmail.com>
2024-05-26 14:46:01 -0300

    fix: env e config for prod

--- config/base.ts
+++ config/base.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:3001";
+export const baseUrl = "https://apilumini.marcelodlg.com.br";
 // export const baseUrl = 'http://decreto.alfenas.mg.gov.br'
 
-export const takeBase = 20;
+export const takeBase = 10;
